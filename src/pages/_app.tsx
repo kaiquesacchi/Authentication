b2f9c4dcc6bb91ddf97a8313faf1243fee0237b0
Kaique Sacchi <sacchikaique@gmail.com>
2021-03-15 19:19:54 -0300

    feat: added Apollo GraphQL client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import { AppProps } from "next/app";
 
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../../lib/apolloClient";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const apolloClient = useApollo(pageProps);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
