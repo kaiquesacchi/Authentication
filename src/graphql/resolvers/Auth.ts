9a623f2a6277ee4e7ab64818142af61272cbef02
Kaique Sacchi <sacchikaique@gmail.com>
2021-03-17 16:57:26 -0300

    feat(api): added Auth-Cookie support
    Based on 'https://dev.to/cvr/graphql-http-only-jwt-authentication-with-next-js-3083'

--- src/graphql/resolvers/Auth.ts
+++ src/graphql/resolvers/Auth.ts
@@ -1,10 +1,12 @@
 import prisma from "../../../lib/prisma";
+import jwt from "jsonwebtoken";
+import { iApolloContext } from "../../pages/api/graphql";
 
 interface iSignIn {
   email: string;
   password: string;
 }
-export async function signIn(_: any, { email, password }: iSignIn) {
+export async function signIn(_: any, { email, password }: iSignIn, context: iApolloContext) {
   const user = await prisma.users.findUnique({
     where: {
       email,
@@ -15,8 +17,13 @@ export async function signIn(_: any, { email, password }: iSignIn) {
   if (user?.password !== password) {
     return null;
   }
-
-  // TODO: Set cookie.
+  let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!);
+  context.cookies.set("auth-token", token, {
+    httpOnly: true,
+    sameSite: "lax",
+    maxAge: 1 * 60 * 60 * 1000, // 12 Hour.
+    secure: process.env.NODE_ENV === "production",
+  });
   return user;
 }
 
--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,9 +1,38 @@
 import { ApolloServer } from "apollo-server-micro";
+import jwt from "jsonwebtoken";
+import Cookies from "cookies";
 
 import { typeDefs } from "../../graphql/schemas";
 import { resolvers } from "../../graphql/resolvers";
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const verifyToken = (token: string | undefined) => {
+  if (!token) return null;
+
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET!) as { id: string };
+  } catch {
+    return null;
+  }
+};
+
+export interface iApolloContext {
+  cookies: Cookies;
+  user: { id: string } | null;
+}
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }): iApolloContext => {
+    const cookies = new Cookies(req, res);
+    const token = cookies.get("auth-token");
+    const user = verifyToken(token);
+    return {
+      cookies,
+      user,
+    };
+  },
+});
 
 // Lets the Apollo Server handle the parsing.
 export const config = {
